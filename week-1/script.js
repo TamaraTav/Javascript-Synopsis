a648a7d9604e0d64d13871b233fbff2c412622b9
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-03 17:43:31 -0500

    add theoretical materials, as well practical assignments in Week-1

---
+++ week-1/script.js
@@ -0,0 +1,59 @@
+// 1.დაწერეთ პროგრამა, რომელიც მომხმარებელს შეეკითხება, რომელ წელს დაიბადა და გამოითვლის მის ასაკს
+/*
+- ვკითხოთ მომხმარებელს, რომელ წელს დაიბადა.
+  - გადავიყვანოთ დაბადების წელი რიცხვითი ტიპის მნიოშვნელობაში.
+
+- გამოითვალოს მომხმარებლის ასაკი.
+  - გავიგოთ რომლი წელია.
+*/
+/*
+let birthYear = Number(prompt('რომელ წელს დაიბადე'));
+let currentYear = 2024;
+let age = currentYear - birthYear;
+console.log(age);
+*/
+// 2.დაწერეთ პროგრამა, რომელიც მომხმარებელს შეაყვანინებს ორ რიცხვს და დაითვლის მათ ჯამს, სხვაობას, ნამრავლსა და განაყოფს.
+/*
+let x = Number(prompt("Enter the first number"));
+let y = Number(prompt("Enter the second number"));
+
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let ganayofi = x / y;
+
+console.log(`ჯამი: ${sum}, სხვაობა: ${difference}, ნამრავლი: ${product}, განაყოფი: ${ganayofi}`)
+*/
+// 3.დაწერეთ პროგრამა, რომელიც მომხმარებელს შეაყვანინებს წრის რადიუსს და დაითვლის წრის ფართობს.
+/*
+let radius = Number(prompt("Enter the raidus: "));
+
+let PI = 3.14;
+let area = PI * radius ** 2;
+console.log(area);
+*/
+// 4. დაწერეთ პროგრამა ორი რიცხვის გაყოფის ნაშთის გამოსათვლელად.
+/*
+let x = 5;
+let y = 3;
+let remainder = x % y;
+console.log(remainder);
+*/
+// 5. შექმენით ცვლადი და მიანიჭეთ მას სტრიქონი. ამობეჭდეთ სტრიქონის სიგრძე.
+/*
+let str = 'AlgoUni';
+let strLength = str.length;
+console.log(strLength);
+*/
+// 6. დაწერეთ პროგრამა სტრიქონის დიდ ასოებზე გადასაყვანად.
+/*
+let text = 'Welcome to AlgoUni';
+let upperCaseText = text.toUpperCase();
+console.log(upperCaseText);
+*/
+// 7. დაწერეთ პროგრამა სტრიქონის მცირე ასოზე გადასაყვანად.
+/*
+let text = 'Welcome to AlgoUni';
+let lowerCaseText = text.toLowerCase();
+console.log(lowerCaseText);
+*/
