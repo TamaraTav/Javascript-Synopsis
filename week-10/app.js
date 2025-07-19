e3b4e218b81a57c0383689839fd6396553f42230
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-19 17:47:28 -0500

    add GIT

---
+++ week-10/app.js
@@ -0,0 +1,16 @@
+// GET
+
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData();
