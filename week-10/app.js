47a5ce2dc005e9391c16365622697c6532e975ae
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-19 18:19:17 -0500

    add POST

--- week-10/app.js
+++ week-10/app.js
@@ -1,10 +1,35 @@
 // GET
 
-async function fetchData() {
+// async function fetchData() {
+//   try {
+//     const response = await fetch("http://localhost:3000/posts");
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// fetchData();
+
+// POST
+async function postData() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const response = await fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "foo",
+        content: "bar",
+      }),
+    });
     if (!response.ok) {
-      throw new Error("Network response was not ok");
+      throw new Error(`Network response was not ok ${response.status}`);
     }
     const data = await response.json();
     console.log(data);
@@ -13,4 +38,4 @@ async function fetchData() {
   }
 }
 
-fetchData();
+postData();
