03a922c33825ffb04e6eae653c88111944ddb5d2
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-25 17:17:02 -0500

    add task 2 in seminari-11.js

--- week-11/semirari-11.js
+++ week-11/semirari-11.js
@@ -2,22 +2,43 @@
 // ფროფერთები და "sayHello" მეთოდი(ეს ყველაფერი კლასშივე უნდა გაუწეროთ).
 // გამოაკონსოლეთ ობიექტის ფროფერთები.
 //
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  sayHello() {
-    return "hi";
-  }
-}
-
-const person = new Person("Alice", 25);
-console.log(person.name);
-console.log(person.age);
-console.log(person.sayHello());
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//
+//   sayHello() {
+//     return "hi";
+//   }
+// }
+//
+// const person = new Person("Alice", 25);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.sayHello());
 
 
 // 2. მემკვიდრეობითობა: შექმენით კლასი "Student", რომელიც იქნება "Person"-ის შთამომავალი.
 // დაამატეთ ახალი "grade" ფროფერთი.
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    sayHello() {
+        return "hi";
+    }
+}
+
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age); // ამ სუპერ ჩაშენებული ფუნქციით ვიძახებთ მშობელს და მემკვიდრეობით მისგან მოგვაქვს
+    this.grade = grade;
+  }
+}
+
+const student = new Student("Alice", 25, 90);
+console.log(student.grade);
