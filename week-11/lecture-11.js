c88f47586c459b64dfc4d536e4db19760a81d4d9
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-25 15:56:30 -0500

    add some coments

--- week-11/lecture-11.js
+++ week-11/lecture-11.js
@@ -1,8 +1,10 @@
 class User {
-    #creditNumber;
+    #creditNumber; //ანუ ჯერ აქ ვქმნი ძიეზის ნიშნით და ქვევით ვიყენებ ძიეზის ნიშნით
     constructor(name, creditNumber) {
         this.name = name;
-        this.#creditNumber = creditNumber;
+        this.#creditNumber = creditNumber; // privet property: დაფარული ფროფერთი,
+                                          // ანუ აქ ვიყენებ იმ შექმნილს და გარედან ამ
+                                         // პროფერთის ვერ მივწვდები
     }
 
     sayHi() {
@@ -10,7 +12,7 @@ class User {
     }
 
     get creditNumber() {
-        return this.#creditNumber;
+        return this.#creditNumber;  // ამის მეშვეობით შიგნიდან მივწვდებით.
     }
 
     set creditNumber(newCreditNumber) {
@@ -21,6 +23,6 @@ class User {
 const user = new User("Alice", "123");
 console.log(user.creditNumber);
 
-user.name = "temo";
+user.name = "Tako";
 user.creditNumber = "456";
 console.log(user.creditNumber);
