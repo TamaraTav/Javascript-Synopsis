7f92373579526714ceff95fc9d3ccbe8944a8be4
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-26 16:15:37 -0500

    add task 4

--- week-11/semirari-11.js
+++ week-11/semirari-11.js
@@ -47,10 +47,35 @@
 // 3. სტატიკური მეთოდი: შექმენით კლასი "Calculator" სტატიკური მეთოდით,
 // რომელიც პარამეტრად მიიღებს ორ რიცხვს და დააბრუნებს მათ ჯამს.
 
-class Calculator {
-    //ეს ქქვედა მეთოდი გამოვა სტატიკური, რადგან ვიყებენ სიტყვას static
-  static sum(a, b) {
-    return a + b;
+// class Calculator {
+//     //ეს ქქვედა მეთოდი გამოვა სტატიკური, რადგან ვიყებენ სიტყვას static
+//   static sum(a, b) {
+//     return a + b;
+//   }
+// }
+// console.log(Calculator.sum(5, 10));
+
+// 4. Getter and Setter: შექმენით კლასი "Temperature" private ფორფერთით - "celsius".
+// დაამატეთ getter და setter მეთოდები. getter მეთოდი უნდა აბრუნებდეს celsius ფროფერთის.
+// setter მეთოდი პარამეტრად უნდა იღებდეს ფარენჰეიტს, გადაჰყავდეს ცელსიუსში და ანიჭებდეს მიღებულ
+// მნიშვნელობას celsius ფროფერთის.
+
+class Temperature {
+  #celsius;
+  constructor(celsius) {
+    this.#celsius = celsius;
+  }
+
+  get celsius() {
+    return this.#celsius;
+  }
+
+  set celsius(fahrenheit) {
+    this.#celsius = ((fahrenheit - 32) * 5) / 9;
   }
 }
-console.log(Calculator.sum(5, 10));
+
+const temperature = new Temperature(25);
+console.log(temperature.celsius);
+temperature.celsius = 77;
+console.log(temperature.celsius);
