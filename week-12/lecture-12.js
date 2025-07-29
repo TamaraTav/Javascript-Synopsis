a3a7ac86ba7a5b128b5c27257267e5fa1df2daa5
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-29 12:50:47 -0500

    Binary Search

--- week-12/lecture-12.js
+++ week-12/lecture-12.js
@@ -24,13 +24,36 @@
 
 
 //Linear Search
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == target) {
-      return i;
+// function linearSearch(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+//
+// console.log(linearSearch([1, 2, 3, 4, 5], 3)); // 2
+
+
+// Binary Search
+function binarySearch(arr, target) {
+    let low = 0;
+    let high = arr.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2); //აქ აუცილებლად პლიუსი უნდა იყოს და არა მინუსი
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
     }
-  }
-  return -1;
+
+    return -1;
 }
 
-console.log(linearSearch([1, 2, 3, 4, 5], 3)); // 2
+console.log(binarySearch([2, 3, 5, 7, 8, 10, 12, 15, 18, 20], 7)); // 3
