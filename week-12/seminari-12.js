f231487cd2bc9215c04ec66b0375b2183f4f87e0
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-31 16:42:17 -0500

    add QuickSort

--- week-12/seminari-12.js
+++ week-12/seminari-12.js
@@ -1,5 +1,5 @@
 // Selection Sort -> O(n^2)
-
+/*
 function selectionSort(arr) {
   if (arr.length <= 1) return arr;
   for (let i = 0; i < arr.length - 1; i++) {
@@ -19,4 +19,31 @@ function selectionSort(arr) {
 }
 
 console.log(selectionSort([10, 22, 34, 19, 17])); // [10, 17, 19, 22, 34]
+*/
+
+
+
+// Quick Sort -> O(n log n)  ეს ბევრად სწრაფია!!!!!!!!!!!
+//ვიყენებ რეკურსიას
+
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let pivot = arr[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)]; //ჩავსფრედავ
+}
+
+console.log(quickSort([3, -3, 5, 2, 6, 8, -6, 1, 9]));
+
 
