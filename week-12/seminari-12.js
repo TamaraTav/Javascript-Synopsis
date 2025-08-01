1810e45ef1e00756d7fa05e323a04a1665a96182
TamaraTav <tamara.tavamaishvili@gmail.com>
2025-07-31 20:17:01 -0500

    Luhn's Algorithm

--- week-12/seminari-12.js
+++ week-12/seminari-12.js
@@ -22,28 +22,91 @@ console.log(selectionSort([10, 22, 34, 19, 17])); // [10, 17, 19, 22, 34]
 */
 
 
+//
+// // Quick Sort -> O(n log n)  ეს ბევრად სწრაფია!!!!!!!!!!!
+// //ვიყენებ რეკურსიას
+//
+// function quickSort(arr) {
+//   if (arr.length <= 1) return arr;
+//
+//   let pivot = arr[0];
+//   let left = [];
+//   let right = [];
+//
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+//
+//   return [...quickSort(left), pivot, ...quickSort(right)]; //ჩავსფრედავ
+// }
+//
+// console.log(quickSort([3, -3, 5, 2, 6, 8, -6, 1, 9]));
 
-// Quick Sort -> O(n log n)  ეს ბევრად სწრაფია!!!!!!!!!!!
-//ვიყენებ რეკურსიას
 
-function quickSort(arr) {
-  if (arr.length <= 1) return arr;
 
-  let pivot = arr[0];
-  let left = [];
-  let right = [];
+// Luhn's  Algorithm- ადგენს ვალიდურია თუ არა კრედიტ-კარტის ნომერი
+// https://cs50.harvard.edu/x/psets/1/credit/   ამ ლინკზე არის აღწერილობა
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
-    }
+function luhnAlgorithm(creditCardNumber) {
+  const creditString = creditCardNumber.toString();
+  let length = creditString.length;
+
+  if (length !== 13 && length !== 15 && length !== 16) {
+    return "Invalid";
   }
 
-  return [...quickSort(left), pivot, ...quickSort(right)]; //ჩავსფრედავ
+  const firstDigit = parseInt(creditString[0]);
+  const firstTwoDigits = parseInt(creditString.slice(0, 2));
+
+  if (
+      firstDigit !== 4 &&
+      firstTwoDigits !== 34 &&
+      firstTwoDigits !== 37 &&
+      (firstTwoDigits < 51 || firstTwoDigits > 55)
+  ) {
+    return "Invalid";
+  }
+
+  let checkSum = 0;
+
+  while (creditCardNumber > 0) {
+    let digit = Math.floor((creditCardNumber % 100) / 10); // ამით ვიღებთ ბოლოდან მეორე რიცხვს
+    let product = digit * 2;
+
+      if (product >= 10) {
+        product = (product % 10) + Math.floor(product / 10);
+      }
+
+    checkSum += product;
+
+    const remainingDigit = creditCardNumber % 10; // ამით ვიღებთ დარჩენილ რიცხვებს,
+    // ანუ ბოლოს, ბოლოდან მესამეს და ა.შ.
+    checkSum += remainingDigit;
+
+    creditCardNumber = Math.floor(creditCardNumber / 100);
+  }
+
+  if (checkSum % 10 !== 0) {
+    return "Invalid";
+  }
+
+  if (length === 15 && (firstTwoDigits === 34 || firstTwoDigits === 37)) {
+    return "AMEX";
+  } else if (length === 16 && firstTwoDigits >= 51 && firstTwoDigits <= 55) {
+    return "MasterCard";
+  } else if ((length === 16 || length === 13) & (firstDigit === 4)) {
+    return "Visa";
+  } else {
+    return "Invalid";
+  }
 }
 
-console.log(quickSort([3, -3, 5, 2, 6, 8, -6, 1, 9]));
+console.time("luhnAlgorithm"); //რა დრო დაჭირდება შესრულებას მაგას ვზომავ
+console.log(luhnAlgorithm(4003600000000014));
+console.timeEnd("luhnAlgorithm");
 
 
